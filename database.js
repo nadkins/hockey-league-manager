2a6d01638f52c43867047bb8e5ab78dae93116f1
MivaLearn <nakins@mivamerchant.com>
2024-05-22 12:54:44 -0700

    update database

--- database.js
+++ database.js
@@ -4,8 +4,8 @@ const mysql = require("mysql");
 const pool = mysql.createPool({
 	connectionLimit: 10,
 	host: process.env.DB_HOST || "localhost", // Use environment variable for DB host or default to localhost
-	user: process.env.DB_USER || "root", // Use environment variable for DB user or default to root
-	password: process.env.DB_PASSWORD || "",
+	user: process.env.DB_USER || "manager", // Use environment variable for DB user or default to root
+	password: process.env.DB_PASSWORD || "Manager@123!",
 	database: process.env.DB_NAME || "sdfhl_site", // Use environment variable for DB name or default to sdfhl_site
 });
 
--- server.js
+++ server.js
@@ -60,8 +60,8 @@ const upload = multer({storage: storage});
 const pool = mysql.createPool({
 	connectionLimit: 10,
 	host: process.env.DB_HOST || "localhost", // Use environment variable for DB host or default to localhost
-	user: process.env.DB_USER || "root", // Use environment variable for DB user or default to root
-	password: process.env.DB_PASSWORD || "",
+	user: process.env.DB_USER || "manager", // Use environment variable for DB user or default to root
+	password: process.env.DB_PASSWORD || "Manager@123!",
 	database: process.env.DB_NAME || "sdfhl_site", // Use environment variable for DB name or default to sdfhl_site
 });
 
